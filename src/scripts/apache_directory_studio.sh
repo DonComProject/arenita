5b4eed2c5b462a67c7575420b936a3726834f80d
DonCom's Project <fredydansiu@gmail.com>
2024-05-15 20:02:46 +0200

    Update apache_directory_studio.sh

--- src/scripts/apache_directory_studio.sh
+++ src/scripts/apache_directory_studio.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+#Instalar OpenJDK, una implementación de código abierto de la plataforma Java.
+sudo apt install default-jdk
+
 # Descargar Apache Directory Studio
 wget https://dlcdn.apache.org/directory/studio/2.0.0.v20210717-M17/ApacheDirectoryStudio-2.0.0.v20210717-M17-linux.gtk.x86_64.tar.gz
 
