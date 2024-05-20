54227d805da4c33e3c4873c806516a508db84832
DonCom's Project <fredydansiu@gmail.com>
2024-05-20 02:48:55 +0200

    Update ldap_client.sh

--- src/scripts/ldap_client.sh
+++ src/scripts/ldap_client.sh
@@ -2,6 +2,9 @@
 sudo apt update
 sudo apt install libpam-ldapd libnss-ldapd ldap-utils
 echo "client" > /etc/hostname
-wget https://raw.githubusercontent.com/DonComProject/arenita/main/src/conf/common-auth > /etc/pam.d/common-auth
+sudo wget https://raw.githubusercontent.com/DonComProject/arenita/main/src/conf/common-auth > /etc/pam.d/common-auth
+sudo wget https://raw.githubusercontent.com/DonComProject/arenita/main/src/conf/common-account > /etc/pam.d/common-account
+sudo wget https://raw.githubusercontent.com/DonComProject/arenita/main/src/conf/common-password > /etc/pam.d/common-password
+sudo wget https://raw.githubusercontent.com/DonComProject/arenita/main/src/conf/common-session > /etc/pam.d/common-session
 sudo systemctl restart nslcd
-wget https://raw.githubusercontent.com/DonComProject/arenita/main/src/conf/nsswitch.conf > /etc/nsswitch.conf
+sudo wget https://raw.githubusercontent.com/DonComProject/arenita/main/src/conf/nsswitch.conf > /etc/nsswitch.conf
