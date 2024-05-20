6649d4e6a0d4603a6a0645ed428595994dd08405
DonCom's Project <fredydansiu@gmail.com>
2024-05-20 17:11:02 +0200

    Create rocket_setup.sh

---
+++ src/programs/rocket/rocket_setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Instalar Python 3
+sudo apt install python3 -y
+
+# Establecer un alias para ejecutar el script main.py
+echo 'alias rocket="python3 $(pwd)/main.py"' >> ~/.bashrc
+
+# Actualizar el archivo .bashrc
+source ~/.bashrc
