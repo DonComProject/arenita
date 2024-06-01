3607ad50bbb8c6d28f919035cc14a629f0ae90d1
mgrl39 <eghribelmohamed@elpuig.xeill.net>
2024-06-01 03:06:52 +0200

    Update ldapclient2.sh

--- src/scripts/ldapclient2.sh
+++ src/scripts/ldapclient2.sh
@@ -21,7 +21,7 @@ echo "slapd slapd/backend string MDB" | sudo debconf-set-selections
 echo "slapd slapd/purge_database boolean false" | sudo debconf-set-selections
 echo "slapd slapd/move_old_database boolean true" | sudo debconf-set-selections
 
-sudo sed -i "s/ldap://IP_SERVER_LDAP/ldap:\/\/$IP_SERVER_LDAP/g" /etc/ldap.conf
+sudo sed -i "s/ldap://IP_SERVER_LDAP/ldap:\/\/$IP_SERVER_LDAP/g" /etc/ldap/ldap.conf
 sudo sed -i "s/ldap://IP_SERVER_LDAP/ldap:\/\/$IP_SERVER_LDAP/g" /etc/nslcd.conf
 
 # Instalar los paquetes slapd y ldap-utils de forma no interactiva
